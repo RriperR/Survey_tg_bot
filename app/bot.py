3fe5f5cf7113cede529a963088d646db0b90fc11
RriperR <pikuci2006@gmail.com>
2025-04-16 10:57:27 +0300

    add: reports

--- app/bot.py
+++ app/bot.py
@@ -1,7 +1,11 @@
+import os
+
 from apscheduler.schedulers.background import BackgroundScheduler
+from dotenv import load_dotenv
 
 from reg import start, handle_name_selection, handle_confirmation, handle_photo
 from survey import *
+from report import send_monthly_report
 
 # Сразу при старте один раз загрузим данные.
 update_local_cache()
@@ -13,6 +17,8 @@ scheduler = BackgroundScheduler()
 scheduler.add_job(update_local_cache, 'cron', hour=19, minute=50)
 
 scheduler.add_job(run_survey_dispatch, 'cron', hour=20, minute=30)
+
+scheduler.add_job(send_monthly_report, 'cron', day=4, hour=20, minute=52)
 
 scheduler.start()
 
